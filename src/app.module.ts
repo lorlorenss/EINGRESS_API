764b617000d04a8c718ad750375699e120f12656
lorlorenss <l.anasco.513898@umindanao.edu.ph>
2024-07-29 08:49:17 +0800

    Added login-total

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { AuthModule } from './auth/auth.module';
 import { AdminLoginModule } from './admin-login/admin-login.module';
 import { EmployeeListModule } from './employee-list/employee-list.module';
 import { AccessLogModule } from './access-log/access-log.module';
-
+import { LoginTotalModule } from './login-total/login-total.module';
 
 @Module({
   // imports: [TypeOrmModule.forRoot(config)],
